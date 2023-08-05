94a7e96c2abfbd457aadedb376e640eb1815cae5
byjuno <byjuno-dev@byjuno.ch>
2023-08-05 14:36:32 +0300

    error url

--- Helper/DataHelper.php
+++ Helper/DataHelper.php
@@ -1435,7 +1435,9 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper
         $request->customerConsents = Array($customerConsents);
 
         $request->merchantDetails->returnUrlError = $this->_urlBuilder->getUrl('cembrapaycheckoutcore/checkout/cancel');
+        $request->merchantDetails->ReturnUrlCancel = $this->_urlBuilder->getUrl('cembrapaycheckoutcore/checkout/cancel');
         $request->merchantDetails->returnUrlSuccess = $this->_urlBuilder->getUrl('checkout/onepage/success');
+
         $request->merchantDetails->transactionChannel = "WEB";
         $request->merchantDetails->integrationModule = "CembraPay Checkout Magento 2 module 0.0.1";
 
